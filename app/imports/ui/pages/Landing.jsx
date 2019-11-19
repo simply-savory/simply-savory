6d13add42dc9c752be7cd10e1c068c7cf83d1e23
unknown <kade.shiroma@gmail.com>
2019-11-19 01:23:14 -1000

    simply savory logo added and landing page changes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, List } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <Grid container centered stackable columns={2}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+          <Grid.Column textAlign="center">
+            <Image size="massive" src="/images/simply-savory-logo.png"/>
+            <p>Simply Savory is a recipe sharing solution that creates a way for students (both on and off campus) to
+              learn and share recipes that:
+            </p>
+            <List>
+              <List.Item>Can be made using minimal kitchen facilities (at a minimum, a toaster oven).</List.Item>
+              <List.Item>Can be made out of ingredients that are available within walking distance of UH.</List.Item>
+              <List.Item>Suit local taste sensibilities.</List.Item>
+              <List.Item>Can be filtered via dietary restrictions (gluten-free, vegan, etc).</List.Item>
+              <List.Item>Have an estimated cost per serving.</List.Item>
+              <List.Item>Has an estimated number of servings per recipe.</List.Item>
+              <List.Item>Has an estimate of how long it takes to make.</List.Item>
+            </List>
           </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
         </Grid>
     );
   }
