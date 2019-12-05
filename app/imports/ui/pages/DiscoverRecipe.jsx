11afe74c5a6398897d7cba1c480241fb3deab659
unknown <kade.shiroma@gmail.com>
2019-12-04 19:35:54 -1000

    fix cards

--- app/imports/ui/pages/DiscoverRecipe.jsx
+++ app/imports/ui/pages/DiscoverRecipe.jsx
@@ -32,8 +32,17 @@ class DiscoverRecipe extends React.Component {
     return (
         <Container>
           <Header as="h2" textAlign="center" inverted>Try these popular recipes</Header>
+          <Input
+              action={{
+                content: 'Search',
+              }}
+              placeholder='Search by name or ingredient'
+              type='text'
+              value={this.state.search}
+              onChange={this.updateSearch.bind(this)}
+          />
           <Card.Group itemsPerRow={4}>
-            {this.props.recipes.map((recipe, index) => <RecipeCard
+            {filteredRecipe.map((recipe, index) => <RecipeCard
                 key={index}
                 recipe={recipe}/>)}
           </Card.Group>
