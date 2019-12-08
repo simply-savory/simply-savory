81a8ea0147b3d157f3075b3840634a44f61a87e5
nchu277 <35511652+nchu277@users.noreply.github.com>
2019-12-07 20:31:49 -1000

    eslint syntax fix

--- app/imports/ui/pages/SearchRecipe.jsx
+++ app/imports/ui/pages/SearchRecipe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Card, Image, Button} from 'semantic-ui-react';
+import { Container, Header, Card, Image, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
